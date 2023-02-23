f6c5868fd9b0862f4e982e723b34000ba3ed85b1
Rafiu92 <abdulrafiusagyardoo68@gmail.com>
2023-02-22 17:03:41 -0800

    104-fibonacci.c_update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 
 /**
  * print _times_table - print the n times table, starting with 0
@@ -7,36 +7,36 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-	    if (n >= 0 && n <= 15)
-	    {
-		    for (i = 0, i <= n, i++)
-		    {
-			   for (j = 0, j <= n, j++)
-			   {
-				   k = j * i;
-				   if (j == 0)
-				   {
-					   _putchar(k + '0');
-				    } else if (k < 10 && j != 0)
-				    {
+       	if (n >= 0 && n <= 15)
+       	{
+	       	for (i = 0, i <= n, i++)
+	       	{
+		       	for (j = 0, j <= n, j++)
+		       	{
+			       	k = j * i;
+			       	if (j == 0)
+			       	{
+				       	_putchar(k + '0');
+			       	} else if (k < 10 && j != 0);
+			       	{
 					    _putchar(',');
 					    _putchar(' ');
 					    _putchar(' ');
 					    _putchar(' ');
 					    _putchar(k + '0');
 				    } else if (k >= 10 && k < 100)
-                                    {
-                                            _putchar(',');
-                                            _putchar(' ');
-                                            _putchar(' ');
-                                            _putchar((k / 10) + '0');
-                                            _putchar((k % 10) + '0');
+				    {
+					    _putchar(',');
+					    _putchar(' ');
+					    _putchar(' ');
+					    _putchar((k / 10) + '0');
+					    _putchar((k % 10) + '0');
 				    } else if (k >= 100)
-                                    {
-                                            _putchar(',');
-                                            _putchar(' ');
-                                            _putchar((k / 100) + '0');
-                                            _putchar((k / 10) % 10 + '0');
+				    {
+					    _putchar(',');
+					    _putchar(' ');
+					    _putchar((k / 100) + '0');
+					    _putchar((k / 10) % 10 + '0');
 					    _putchar((k % 10) + '0');
 				    }
 			   }
